b73a484c884fcccec006ad8b037b77f137a6e8d4
Otso Jousimaa <otso@ojousima.net>
2017-08-21 07:23:37 +0000

    Add comment about MAM-js libs

--- example.js
+++ example.js
@@ -1,5 +1,12 @@
 var BleUart = require('./ble-uart');
-var parser = require('./ruuvi.endpoints.js/index.js')
+var dataHandler = require('./ruuvi.endpoints.js/index.js')
+var tryteConverter = require('./iota.lib.js/lib/utils/asciiToTrytes.js')
+var MAM = require('./iota-mam/mam')
+
+https://stackoverflow.com/questions/6965107/converting-between-strings-and-arraybuffers
+function ab2str(buf) {
+  return String.fromCharCode.apply(null, new Uint8Array(buf));
+}
 
 // use a predefined UART service (nordic, redbear, laird, bluegiga)
 var bleSerial = new BleUart('nordic');
@@ -16,16 +23,29 @@ var bleSerial = new BleUart('nordic');
 // the Bluetooth LE serial service:
 bleSerial.on('data', function(data){
   //console.log("Got new data: " + data);
-  parser(data);
+  let message = dataHandler(data);
+  if (message.ready){
+    let res = (ab2str(message.binary)).split('\n');
+    console.log(res[0]);
+    console.log(res[1]);
+    //if(message.type === "MAM")
+    //{
+      let trytes = MAM.mam_parse(res[0], res[1], 1).split('\n');
+    //}
+    console.log(trytes);
+    let ascii = tryteConverter.fromTrytes(trytes[0]);
+    console.log(ascii);
+  }
 });
 
 // this function gets called when the program
 // establishes a connection with the remote BLE radio:
 bleSerial.on('connected', function(data){
   console.log("Connected to BLE. Sending a hello message");
-  bleSerial.write("Hello BLE!");
+  
+  //bleSerial.write("Hello BLE!");
   //bleSerial.write([1,2,3,4,5]);
-  //bleSerial.write(new Uint8Array([5,4,3,2,1]));
+  bleSerial.write(new Uint8Array([0x31,40,30,20,10,00,01,02,03,04,05]));
   //bleSerial.write(new Buffer([6,7,8,9]))
 });
 
--- readme.md
+++ readme.md
@@ -1,8 +1,29 @@
-# Dependencies
+#Usage
 
+After installing, run node example.js
+Program searches for RuuviTag which has Nordic UART service enabled
+
+After connection, a non-sense message is sent to TEMPERATURE enpoint, RuuviTag will reply with unknown-message.
+If RuuviTag sends a MAM message, MAM message is parsed and printed on console. 
+MAM message parsing requres MAM-js libraries which aren't published yet.
+
+# Dependencies
 ## Noble
+[https://github.com/sandeepmistry/noble]
 
 ## iota.lib.js
+Git submodule, init & update
+
+#ruuvi.endpoints.js
+Git submodule, init & update
 
 ## ble-uart
+[https://github.com/tigoe/BluetoothLE-Examples]
+Install Noble->Readserial
+
+#Licensing
+Ruuvi / Ojousima: BSD-3
+iota libs: MIT
+BLE-UART: Unknown, please refer to [https://github.com/tigoe/BluetoothLE-Examples/issues/7]
+Noble: MIT 
 
