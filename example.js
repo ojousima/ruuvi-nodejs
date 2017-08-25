826f2be71e2fce88bda194b8c559c0879fedc19c
Otso Jousimaa <otso@ojousima.net>
2017-08-25 05:17:56 -0500

    Add example commandline UI

--- example.js
+++ example.js
@@ -1,8 +1,14 @@
+const util = require('util');
+const setImmediatePromise = util.promisify(setImmediate);
 var BleUart = require('./ble-uart');
 var dataHandler = require('./ruuvi.endpoints.js/index.js')
 var tryteConverter = require('./iota.lib.js/lib/utils/asciiToTrytes.js')
 var MAM = require('./iota-mam/mam')
 var sleep = require('sleep');
+var stdin = process.openStdin();
+var singleShotTemperature = new Uint8Array([0x31,0x10,0x01,251,251,252,252,1,0,2,0]);
+var continuousTemperature = new Uint8Array([0x31,0x10,0x01,1,251,252,252,1,0,2,0]);
+var queryTemperature      = new Uint8Array([0x31,0x10,0x05,0,0,0,0,0,0,0,0]);  
 
 https://stackoverflow.com/questions/6965107/converting-between-strings-and-arraybuffers
 function ab2str(buf) {
@@ -43,11 +49,10 @@ bleSerial.on('data', function(data){
 // establishes a connection with the remote BLE radio:
 bleSerial.on('connected', function(data){
   console.log("Connected to BLE. Sending a hello message");  
+  timerExample();
   //bleSerial.write("Hello BLE!");
   //bleSerial.write([1,2,3,4,5]);
-  bleSerial.write(new Uint8Array([0x31,0x10,0x01,251,251,252,252,1,0,3,0]));
-  sleep.sleep(5);
-  bleSerial.write(new Uint8Array([0x31,0x10,0x05,0,0,0,0,0,0,0,0]));
+  
 
   //bleSerial.write(new Buffer([6,7,8,9]))
 });
@@ -56,3 +61,33 @@ bleSerial.on('connected', function(data){
 bleSerial.on('scanning', function(status){
   console.log("radio status: " + status);
 })
+
+async function timerExample() {
+  //console.log('Before I/O callbacks');
+  await setImmediatePromise();
+  console.log('Connection ready.');
+  console.log('1) singleshot 2) continous 3)  query.');
+  //bleSerial.write(new Uint8Array([0x31,0x10,0x01,251,251,252,252,1,0,2,0]));
+  //sleep.sleep(5);
+  //bleSerial.write(new Uint8Array([0x31,0x10,0x05,0,0,0,0,0,0,0,0]));  
+}
+
+stdin.on('data', function(chunk) { 
+  if(chunk == '1\n')
+  {
+     console.log("Single shot"); 
+     bleSerial.write(singleShotTemperature);
+  }
+  if(chunk == '2\n')
+  {
+    console.log("Continous measurement");   
+    bleSerial.write(continuousTemperature);
+  }
+  if(chunk == '3\n')
+  {
+    console.log("Query data"); 
+    bleSerial.write(queryTemperature);
+  }
+  }
+);
+
